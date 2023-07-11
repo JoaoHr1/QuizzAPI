c1ceb14196c357b86c68027f84def651e592fc97
Joao Henrique <joaoibitira@gmail.com>
2023-07-11 19:53:26 -0300

    initial commit

---
+++ src/main/java/dev/joao/QuizzAPI/QuizzApiApplication.java
@@ -0,0 +1,13 @@
+package dev.joao.QuizzAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joao/QuizzAPI/QuizzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joao.QuizzAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
